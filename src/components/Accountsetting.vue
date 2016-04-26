c73d31f45b672fe02fe8abcea6ee1a1681505ae1
nattapat sukpootanan <nattapat.su@ku.th>
2016-04-26 21:30:50 +0700

    Test .vue

---
+++ src/components/Accountsetting.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-sm-4 col-sm-offset-4">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h1 class="panel-title">General Account Settings</h1>
+      </div>
+      <div class="panel-body">
+        <h5>Community of the people who has car</h5>
+        username
+        <div class="form-group">
+          <input type="text"class="form-control"placeholder="username"v-model="username">
+        </div>
+        <p>Email Address</p>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="E-mail" v-model="email">
+        </div>
+        <p>Password</p>
+        <div class="form-group">
+          <input type="password" class="form-control" placeholder="password" v-model="password">
+        </div>
+        <div class="row">
+        <div class="col-md-6">
+        <button class="btn btn-primary btn-block" @click="method()">Save changes</button>
+        <button class="btn btn-default btn-block" @click="method()">Cancel</button>
+      </div>
+    </div>
+    </div>
+  </div>
+
+</template>
---
+++ src/components/Test.vue
@@ -0,0 +1,33 @@
+<!-- page for testing -->
+<template>
+  <div class="col-sm-4 col-sm-offset-4">
+    <h1>Tabien Testing Page</h1>
+    <input type="text" class="form-control" placeholder="Input something" v-model="msg" >
+    <button class="btn btn-primary btn-block" @click="click()">somebutton</button>
+
+    <h3>You can see text here.</h3>
+    {{showText}}
+    <br>
+    <h4>this is text in input</h4>
+    {{msg}}
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg:"",
+      showText:""
+    }
+  },
+  methods: {
+    click(){
+      //do something
+      this.showText = "click";
+      this.msg="";
+    }
+  }
+}
+</script>
